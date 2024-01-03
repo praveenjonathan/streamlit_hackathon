e9f621210abd408a1d782980bc3a50f1ec3e5ab6
Danamma Hiremath <98955573+97Danu@users.noreply.github.com>
2024-01-03 18:03:35 +0000

    UPDATED HOME PAGE NAME

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.set_page_config(
 
 show_pages(
           [
-              Page("main.py", "Home", "🏠"),
+              Page("main.py", "START FORM HERE", "🏠"),
               Page("pages/dropout_rate.py", "Female Drop out rates", "1️⃣"),
             #   Page("pages/Leet1321.py", "Restaurant Growth", "2️⃣"),
             #   Page("pages/Leet185.py", "Department Top Three Salaries", "3️⃣"),
