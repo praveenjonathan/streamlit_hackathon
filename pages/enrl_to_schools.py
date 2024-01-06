f522d232af0e9740f4ba6c250d924d772002fd80
Danamma Hiremath <98955573+97Danu@users.noreply.github.com>
2024-01-06 15:06:45 +0000

    insights

--- pages/enrl_to_schools.py
+++ pages/enrl_to_schools.py
@@ -169,6 +169,24 @@ def main():
 
         # Displaying the chart using Streamlit
     st.altair_chart(chart, use_container_width=True)
+    r4_expander = st.expander("Insights from 2013 to 2016**")
+    r4_expander.markdown("1. **During 2015-16:**")
+    r4_expander.markdown("- **GER in Higher Secondary Schools:** Boys - 55.95, Girls - 56.41, Total - 56.16")
+    r4_expander.markdown("- **States/UTs below Nation value (56.16):**")
+    r4_expander.markdown("  - Daman & Diu (21.54), Bihar (35.62), Nagaland (36.43), Odisha (36.54), Assam (38.81), Karnataka (39.86), Meghalaya (43.35), Gujarat (43.43), Tripura (43.46), Madhya Pradesh (45.25), Jharkhand (48.32), Dadra & Nagar Haveli (48.49), West Bengal (51.54), Chhattisgarh (54), Mizoram (55.68)")
+    r4_expander.markdown("- **States/UTs above Nation value (56.16):**")
+    r4_expander.markdown("  - Lakshadweep (98.16), Himachal Pradesh (95.53), Chandigarh (83.28), Tamil Nadu (82.03), Delhi (77.9), Kerala (77.56), Goa (75.84), Uttarakhand (75.83), Puducherry (74.8), Andaman & Nicobar Islands (74.62), Punjab (70.19), Sikkim (68.23), Manipur (67.95), Maharashtra (67.81), Arunachal Pradesh (61.81), Telangana (61.32), Uttar Pradesh (60.78), Andhra Pradesh (60.16), Haryana (59.59), Rajasthan (59.31), Jammu & Kashmir (58.6)")
+
+    r4_expander.markdown("2. **During 2014-15:**")
+    r4_expander.markdown("- **GER in Higher Secondary Schools:** Boys - 54.57, Girls - 53.81, Total - 54.21")
+    r4_expander.markdown("- **States/UTs below Nation value (54.21):**")
+    r4_expander.markdown("  - Bihar, Karnataka, Nagaland, Assam, Meghalaya, Daman & Diu, Dadra & Nagar Haveli, Tripura, Gujarat, Madhya Pradesh, Jharkhand, West Bengal, Andhra Pradesh")
+
+    r4_expander.markdown("3. **During 2013-14:**")
+    r4_expander.markdown("- **GER in Higher Secondary Schools:** Boys - 52.77, Girls - 51.58, Total - 52.21")
+    r4_expander.markdown("- **States/UTs below Nation value (52.21):**")
+    r4_expander.markdown("  - Karnataka, Meghalaya, Bihar, Assam, Nagaland, Dadra & Nagar Haveli, Tripura, Jharkhand, Daman & Diu, Madhya Pradesh, West Bengal, Gujarat, Jammu & Kashmir")
+	
     st.markdown("""---------------------------------""")
 
     
