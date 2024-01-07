bceb68a681bfbce1ad66af00d392a643607fb844
Danamma Hiremath <98955573+97Danu@users.noreply.github.com>
2024-01-07 17:42:37 +0000

    LOAN_COUNT

--- pages/4_📊_ALL_INDIA_SURVEY_ON_HIGHER_EDUCATION.py
+++ pages/4_📊_ALL_INDIA_SURVEY_ON_HIGHER_EDUCATION.py
@@ -155,6 +155,90 @@ def main():
     st.altair_chart(chart, use_container_width=True)
  
     st.markdown("""---------------------------------""")
+    st.title("3.Count of students who got Education Loan across India in 2015-16")
+    col5,col6=st.columns(2)
+    with col5:
+        ais_loan_options = ["Standalone Institutes" ,"Universities" ]
+        ais_loan_index = ais_loan_options.index("Colleges")
+        ais_loan_edu= st.selectbox('Select Education type:', options=ais_loan_options, index=ais_loan_index)
+        selected_items = f"Education type: {ais_loan_edu} "
+
+    with col5:
+        ais_loan_col_options = ["TOTAL_GENERAL_TOTAL", "TOTAL_GENERAL_FEMALES",
+                                "TOTAL_SC_TOTAL", "TOTAL_SC_FEMALES",
+                                "TOTAL_ST_TOTAL", "TOTAL_ST_FEMALES",
+                                "TOTAL_OBC_TOTAL", "TOTAL_OBC_FEMALES",
+                                "TOTAL_TOTAL_PERSONS", "TOTAL_TOTAL_FEMALES",
+                                "PWD_GENERAL_TOTAL", "PWD_GENERAL_FEMALES",
+                                "PWD_SC_TOTAL", "PWD_SC_FEMALES",
+                                "PWD_ST_TOTAL", "PWD_ST_FEMALES",
+                                "PWD_OBC_TOTAL", "PWD_OBC_FEMALES",
+                                "PWD_TOTAL_PERSONS", "PWD_TOTAL_FEMALES",
+                                "MUSLIM_MINORITY_GENERAL_TOTAL", "MUSLIM_MINORITY_GENERAL_FEMALES",
+                                "MUSLIM_MINORITY_SC_TOTAL", "MUSLIM_MINORITY_SC_FEMALES",
+                                "MUSLIM_MINORITY_ST_TOTAL", "MUSLIM_MINORITY_ST_FEMALES",
+                                "MUSLIM_MINORITY_OBC_TOTAL", "MUSLIM_MINORITY_OBC_FEMALES",
+                                "MUSLIM_MINORITY_TOTAL_PERSONS", "MUSLIM_MINORITY_TOTAL_FEMALES",
+                                "OTHER_MINORITY_GENERAL_TOTAL", "OTHER_MINORITY_GENERAL_FEMALES",
+                                "OTHER_MINORITY_SC_TOTAL", "OTHER_MINORITY_SC_FEMALES",
+                                "OTHER_MINORITY_ST_TOTAL", "OTHER_MINORITY_ST_FEMALES",
+                                "OTHER_MINORITY_OBC_TOTAL", "OTHER_MINORITY_OBC_FEMALES",
+                                "OTHER_MINORITY_TOTAL_PERSONS", "OTHER_MINORITY_TOTAL_FEMALES"]
+        ais_loan_col_index = ais_loan_col_options.index("PWD_TOTAL_FEMALES")
+        ais_loan_col = st.selectbox('Select cast of category :', options=ais_loan_col_options, index=ais_loan_col_index)
+    st.title(selected_items)
+    Q3 = f''' WITH CTE AS 
+                (SELECT STATES,TOTAL_GENERAL_TOTAL LOAN_COUNT 
+                    FROM V01_AISHE_EDUCATION_TYPE_LOANS_2015_2016 WHERE  EDUCATION_TYPE= '{ais_loan_edu}' 
+                    )           
+                SELECT INDIA_STATES.STATES,CTE.LOAN_COUNT  FROM INDIA_STATES 
+                    LEFT JOIN CTE ON (CTE.STATES=INDIA_STATES.STATES)'''
+    R3 = execute_query(Q3)
+
+    r3_expander = st.expander("Data sets used in this analysis")
+    R3_DF = pd.DataFrame(R3)
+    R3_DF.index = R3_DF.index + 1
+    r3_expander.write(R3_DF)
+
+    india_states_shp = 'https://github.com/97Danu/Maps_with_python/raw/master/india-polygon.shp'
+    india_states = gpd.read_file(india_states_shp)
+    india_states.rename(columns={'st_nm': 'STATES'}, inplace=True)
+
+    # Merge dropout rates with GeoDataFrame
+    merged_data = india_states.merge(R3_DF, how='left', on='STATES')
+
+    # Define bins and labels
+    bins = [float('-inf'), 100, 1000, float('inf')]
+    labels = ['Below 100', '100 - 1000', 'Above 1000']
+
+    # Assigning values to bins and handling 'NA' values
+    conditions = [
+        merged_data['LOAN_COUNT'] < 100,
+        (merged_data['COUNT'] >= 100) & (merged_data['LOAN_COUNT'] <= 1000),
+        merged_data['LOAN_COUNT'] > 1000
+    ]
+
+    # Assigning labels
+    merged_data['color'] = np.select(conditions, labels, default='NA')
+
+    # Create a plotly figure with categorical colors
+    fig = px.choropleth_mapbox(merged_data, geojson=merged_data.geometry, locations=merged_data.index,
+                            color='color',
+                            color_discrete_map={'Below 100': 'Green', '100 - 1000': 'Blue', 'Above 1000': 'Red', 'NA': 'Yellow'},
+                            mapbox_style="carto-positron",
+                            hover_data={'STATES': True, 'LOAN_COUNT': True},
+                            center={"lat": 20.5937, "lon": 78.9629},
+                            zoom=3,
+                            opacity=0.5)
+    
+
+# Update layout for better visualization
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, mapbox={'center': {'lat': 20, 'lon': 78}})
+
+# Display the map in Streamlit
+    st.plotly_chart(fig)
+
+    st.markdown("""---------------------------------""")
     
     
 
